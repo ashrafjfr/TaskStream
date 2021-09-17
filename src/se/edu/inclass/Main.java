a4429069d4c7585a8df16f60fbe5328cb4b9b887
Ashraf Jaffar <a.ashrafjfr@gmail.com>
2021-09-17 17:14:52 +0800

    Add more code

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,6 +21,8 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDeadlinesUsingStream(tasksData);
+        System.out.println("Total number of deadlines (using stream): " + countDeadlinesUsingStream(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
